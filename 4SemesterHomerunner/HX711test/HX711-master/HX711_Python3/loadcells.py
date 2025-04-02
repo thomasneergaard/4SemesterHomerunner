73ba6b4a0c24dfc03627d75462dcc103007defa1
AugustAmdisen98 <150430199+AugustAmdisen98@users.noreply.github.com>
2025-04-02 10:56:41 +0200

    Files via upload from pi

---
+++ 4SemesterHomerunner/HX711test/HX711-master/HX711_Python3/loadcells.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+from hx711 import HX711
+
+GPIO.setmode(GPIO.BCM)
+
+hx = HX711(dout_pin=6, pd_sck_pin=5)
+
+hx.zero()
+input ('Sæt en kendt vægt på sensoren og tryk Enter:')
+reading = hx.get_data_mean(readings=100)
+
+known_weight_grams = input ('Indtast den kendte vægt og tryk Enter:')
+value = float(known_weight_grams)
+
+ratio = reading/value
+hx.set_scale_ratio(ratio)
+
+while True: 
+     weight = hx.get_weight_mean()
+     print(weight)
